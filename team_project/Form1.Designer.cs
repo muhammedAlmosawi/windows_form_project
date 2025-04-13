25b083e8aea299ebd25ff40ed6b7a4096b1c78f3
Mohammed <Muhammadaliraqi52@gmail.com>
2025-04-14 00:57:37 +0300

    first commit

---
+++ team_project/Form1.Designer.cs
@@ -0,0 +1,379 @@
+﻿namespace team_project
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+			this.label1 = new System.Windows.Forms.Label();
+			this.label2 = new System.Windows.Forms.Label();
+			this.label4 = new System.Windows.Forms.Label();
+			this.label5 = new System.Windows.Forms.Label();
+			this.label6 = new System.Windows.Forms.Label();
+			this.comboBox1 = new System.Windows.Forms.ComboBox();
+			this.label3 = new System.Windows.Forms.Label();
+			this.label7 = new System.Windows.Forms.Label();
+			this.panel1 = new System.Windows.Forms.Panel();
+			this.label8 = new System.Windows.Forms.Label();
+			this.textBox1 = new System.Windows.Forms.TextBox();
+			this.label9 = new System.Windows.Forms.Label();
+			this.textBox2 = new System.Windows.Forms.TextBox();
+			this.label10 = new System.Windows.Forms.Label();
+			this.textBox3 = new System.Windows.Forms.TextBox();
+			this.label11 = new System.Windows.Forms.Label();
+			this.textBox4 = new System.Windows.Forms.TextBox();
+			this.label12 = new System.Windows.Forms.Label();
+			this.textBox5 = new System.Windows.Forms.TextBox();
+			this.label13 = new System.Windows.Forms.Label();
+			this.label14 = new System.Windows.Forms.Label();
+			this.textBox6 = new System.Windows.Forms.TextBox();
+			this.label15 = new System.Windows.Forms.Label();
+			this.label16 = new System.Windows.Forms.Label();
+			this.textBox7 = new System.Windows.Forms.TextBox();
+			this.label17 = new System.Windows.Forms.Label();
+			this.SuspendLayout();
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Font = new System.Drawing.Font("Microsoft YaHei UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label1.Location = new System.Drawing.Point(70, 6);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(139, 22);
+			this.label1.TabIndex = 0;
+			this.label1.Text = "Republic of Iraq\r\n";
+			// 
+			// label2
+			// 
+			this.label2.AutoSize = true;
+			this.label2.Font = new System.Drawing.Font("Microsoft YaHei UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label2.Location = new System.Drawing.Point(39, 28);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(229, 42);
+			this.label2.TabIndex = 1;
+			this.label2.Text = "Ministry of Higher Education\r\n   and Scientific Research";
+			this.label2.Click += new System.EventHandler(this.label2_Click);
+			// 
+			// label4
+			// 
+			this.label4.AutoSize = true;
+			this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label4.Location = new System.Drawing.Point(50, 96);
+			this.label4.Name = "label4";
+			this.label4.Size = new System.Drawing.Size(190, 20);
+			this.label4.TabIndex = 3;
+			this.label4.Text = "choose your departement";
+			// 
+			// label5
+			// 
+			this.label5.AutoSize = true;
+			this.label5.Font = new System.Drawing.Font("Akhbar MT", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(178)));
+			this.label5.Location = new System.Drawing.Point(638, -3);
+			this.label5.Name = "label5";
+			this.label5.Size = new System.Drawing.Size(100, 33);
+			this.label5.TabIndex = 4;
+			this.label5.Text = "جمهورية العراق";
+			// 
+			// label6
+			// 
+			this.label6.AutoSize = true;
+			this.label6.Font = new System.Drawing.Font("Microsoft YaHei UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label6.Location = new System.Drawing.Point(12, 70);
+			this.label6.Name = "label6";
+			this.label6.Size = new System.Drawing.Size(294, 22);
+			this.label6.TabIndex = 5;
+			this.label6.Text = "University of Babylon-College of IT";
+			// 
+			// comboBox1
+			// 
+			this.comboBox1.FormattingEnabled = true;
+			this.comboBox1.Items.AddRange(new object[] {
+            "Software departement",
+            "Cybersecuirty deparetement",
+            "networks departement"});
+			this.comboBox1.Location = new System.Drawing.Point(63, 124);
+			this.comboBox1.Name = "comboBox1";
+			this.comboBox1.Size = new System.Drawing.Size(152, 21);
+			this.comboBox1.TabIndex = 6;
+			this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+			// 
+			// label3
+			// 
+			this.label3.AutoSize = true;
+			this.label3.Font = new System.Drawing.Font("Akhbar MT", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(178)));
+			this.label3.Location = new System.Drawing.Point(592, 29);
+			this.label3.Name = "label3";
+			this.label3.Size = new System.Drawing.Size(196, 33);
+			this.label3.TabIndex = 7;
+			this.label3.Text = "وزارة التعليم العالي والبحث العلمي";
+			// 
+			// label7
+			// 
+			this.label7.AutoSize = true;
+			this.label7.Font = new System.Drawing.Font("Akhbar MT", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(178)));
+			this.label7.Location = new System.Drawing.Point(583, 68);
+			this.label7.Name = "label7";
+			this.label7.Size = new System.Drawing.Size(211, 33);
+			this.label7.TabIndex = 8;
+			this.label7.Text = "جامعة بابل-كلية تكنولوجيا المعلومات";
+			// 
+			// panel1
+			// 
+			this.panel1.Location = new System.Drawing.Point(5, 150);
+			this.panel1.Name = "panel1";
+			this.panel1.Size = new System.Drawing.Size(790, 53);
+			this.panel1.TabIndex = 10;
+			this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+			// 
+			// label8
+			// 
+			this.label8.AutoSize = true;
+			this.label8.Location = new System.Drawing.Point(192, 210);
+			this.label8.Name = "label8";
+			this.label8.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+			this.label8.Size = new System.Drawing.Size(32, 13);
+			this.label8.TabIndex = 11;
+			this.label8.Text = "العدد:";
+			// 
+			// textBox1
+			// 
+			this.textBox1.Location = new System.Drawing.Point(84, 208);
+			this.textBox1.Name = "textBox1";
+			this.textBox1.Size = new System.Drawing.Size(100, 20);
+			this.textBox1.TabIndex = 12;
+			// 
+			// label9
+			// 
+			this.label9.AutoSize = true;
+			this.label9.Location = new System.Drawing.Point(192, 241);
+			this.label9.Name = "label9";
+			this.label9.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+			this.label9.Size = new System.Drawing.Size(41, 13);
+			this.label9.TabIndex = 13;
+			this.label9.Text = "التاريخ:";
+			// 
+			// textBox2
+			// 
+			this.textBox2.Location = new System.Drawing.Point(84, 238);
+			this.textBox2.Name = "textBox2";
+			this.textBox2.Size = new System.Drawing.Size(100, 20);
+			this.textBox2.TabIndex = 14;
+			// 
+			// label10
+			// 
+			this.label10.AutoSize = true;
+			this.label10.Location = new System.Drawing.Point(553, 274);
+			this.label10.Name = "label10";
+			this.label10.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+			this.label10.Size = new System.Drawing.Size(28, 13);
+			this.label10.TabIndex = 15;
+			this.label10.Text = "الى/";
+			// 
+			// textBox3
+			// 
+			this.textBox3.Location = new System.Drawing.Point(274, 268);
+			this.textBox3.Name = "textBox3";
+			this.textBox3.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+			this.textBox3.Size = new System.Drawing.Size(275, 20);
+			this.textBox3.TabIndex = 16;
+			this.textBox3.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+			// 
+			// label11
+			// 
+			this.label11.AutoSize = true;
+			this.label11.Location = new System.Drawing.Point(533, 301);
+			this.label11.Name = "label11";
+			this.label11.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+			this.label11.Size = new System.Drawing.Size(17, 13);
+			this.label11.TabIndex = 17;
+			this.label11.Text = "م/";
+			// 
+			// textBox4
+			// 
+			this.textBox4.Location = new System.Drawing.Point(298, 294);
+			this.textBox4.Name = "textBox4";
+			this.textBox4.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+			this.textBox4.Size = new System.Drawing.Size(231, 20);
+			this.textBox4.TabIndex = 18;
+			this.textBox4.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+			// 
+			// label12
+			// 
+			this.label12.AutoSize = true;
+			this.label12.Location = new System.Drawing.Point(710, 324);
+			this.label12.Name = "label12";
+			this.label12.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+			this.label12.Size = new System.Drawing.Size(50, 13);
+			this.label12.TabIndex = 19;
+			this.label12.Text = "تحيه طيبة";
+			// 
+			// textBox5
+			// 
+			this.textBox5.Location = new System.Drawing.Point(63, 347);
+			this.textBox5.Multiline = true;
+			this.textBox5.Name = "textBox5";
+			this.textBox5.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+			this.textBox5.Size = new System.Drawing.Size(663, 221);
+			this.textBox5.TabIndex = 20;
+			// 
+			// label13
+			// 
+			this.label13.AutoSize = true;
+			this.label13.Location = new System.Drawing.Point(369, 596);
+			this.label13.Name = "label13";
+			this.label13.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+			this.label13.Size = new System.Drawing.Size(55, 13);
+			this.label13.TabIndex = 21;
+			this.label13.Text = "مع الاحترام";
+			// 
+			// label14
+			// 
+			this.label14.AutoSize = true;
+			this.label14.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label14.Location = new System.Drawing.Point(657, 625);
+			this.label14.Name = "label14";
+			this.label14.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+			this.label14.Size = new System.Drawing.Size(46, 13);
+			this.label14.TabIndex = 22;
+			this.label14.Text = "المرفقات";
+			// 
+			// textBox6
+			// 
+			this.textBox6.Location = new System.Drawing.Point(525, 650);
+			this.textBox6.Multiline = true;
+			this.textBox6.Name = "textBox6";
+			this.textBox6.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+			this.textBox6.Size = new System.Drawing.Size(201, 392);
+			this.textBox6.TabIndex = 23;
+			// 
+			// label15
+			// 
+			this.label15.AutoSize = true;
+			this.label15.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label15.Location = new System.Drawing.Point(88, 727);
+			this.label15.Name = "label15";
+			this.label15.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+			this.label15.Size = new System.Drawing.Size(82, 13);
+			this.label15.TabIndex = 24;
+			this.label15.Text = "                         ";
+			// 
+			// label16
+			// 
+			this.label16.AutoSize = true;
+			this.label16.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label16.Location = new System.Drawing.Point(88, 762);
+			this.label16.Name = "label16";
+			this.label16.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+			this.label16.Size = new System.Drawing.Size(82, 13);
+			this.label16.TabIndex = 25;
+			this.label16.Text = "                         ";
+			// 
+			// textBox7
+			// 
+			this.textBox7.Location = new System.Drawing.Point(81, 794);
+			this.textBox7.Name = "textBox7";
+			this.textBox7.Size = new System.Drawing.Size(100, 20);
+			this.textBox7.TabIndex = 26;
+			// 
+			// label17
+			// 
+			this.label17.AutoSize = true;
+			this.label17.Location = new System.Drawing.Point(672, 112);
+			this.label17.Name = "label17";
+			this.label17.Size = new System.Drawing.Size(41, 13);
+			this.label17.TabIndex = 27;
+			this.label17.Text = "label17";
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(800, 1066);
+			this.Controls.Add(this.label17);
+			this.Controls.Add(this.textBox7);
+			this.Controls.Add(this.label16);
+			this.Controls.Add(this.label15);
+			this.Controls.Add(this.textBox6);
+			this.Controls.Add(this.label14);
+			this.Controls.Add(this.label13);
+			this.Controls.Add(this.textBox5);
+			this.Controls.Add(this.label12);
+			this.Controls.Add(this.textBox4);
+			this.Controls.Add(this.label11);
+			this.Controls.Add(this.textBox3);
+			this.Controls.Add(this.label10);
+			this.Controls.Add(this.textBox2);
+			this.Controls.Add(this.label9);
+			this.Controls.Add(this.textBox1);
+			this.Controls.Add(this.label8);
+			this.Controls.Add(this.panel1);
+			this.Controls.Add(this.label7);
+			this.Controls.Add(this.label3);
+			this.Controls.Add(this.comboBox1);
+			this.Controls.Add(this.label6);
+			this.Controls.Add(this.label5);
+			this.Controls.Add(this.label4);
+			this.Controls.Add(this.label2);
+			this.Controls.Add(this.label1);
+			this.Name = "Form1";
+			this.Text = "Form1";
+			this.Load += new System.EventHandler(this.Form1_Load);
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+        }
+
+		#endregion
+
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.Label label4;
+		private System.Windows.Forms.Label label5;
+		private System.Windows.Forms.Label label6;
+		private System.Windows.Forms.ComboBox comboBox1;
+		private System.Windows.Forms.Label label3;
+		private System.Windows.Forms.Label label7;
+		private System.Windows.Forms.Panel panel1;
+		private System.Windows.Forms.Label label8;
+		private System.Windows.Forms.TextBox textBox1;
+		private System.Windows.Forms.Label label9;
+		private System.Windows.Forms.TextBox textBox2;
+		private System.Windows.Forms.Label label10;
+		private System.Windows.Forms.TextBox textBox3;
+		private System.Windows.Forms.Label label11;
+		private System.Windows.Forms.TextBox textBox4;
+		private System.Windows.Forms.Label label12;
+		private System.Windows.Forms.TextBox textBox5;
+		private System.Windows.Forms.Label label13;
+		private System.Windows.Forms.Label label14;
+		private System.Windows.Forms.TextBox textBox6;
+		private System.Windows.Forms.Label label15;
+		private System.Windows.Forms.Label label16;
+		private System.Windows.Forms.TextBox textBox7;
+		private System.Windows.Forms.Label label17;
+	}
+}
+
---
+++ team_project/Form1.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Text;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace team_project
+{
+    public partial class Form1: Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+		private void Form1_Load(object sender, EventArgs e)
+		{
+			PrivateFontCollection privateFontCollection = new PrivateFontCollection();
+			privateFontCollection.AddFontFile("thuluth.ttf");
+			FontFamily ff = privateFontCollection.Families[0];
+			label3.Font = new Font(ff, 14);
+			label5.Font = new Font(ff, 14);
+		}
+		private void label2_Click(object sender, EventArgs e)
+		{
+			
+		}
+		private void panel1_Paint(object sender, PaintEventArgs e)
+		{
+			using (Pen pen = new Pen(Color.Black, 1))
+			{
+				// Draw a horizontal line (x1, y1, x2, y2)
+				e.Graphics.DrawLine(pen, 0.0f, 0.0f, 300.0f, 0.0f);
+				e.Graphics.DrawLine(pen, 300.0f, 0.0f, 350.0f, 50f);
+				e.Graphics.DrawLine(pen, 350f, 50f, 530f, 50f);
+				e.Graphics.DrawLine(pen, 530f, 50f, 580f, 0f);
+				e.Graphics.DrawLine(pen, 580f, 0f, 1000, 0f);
+			}
+
+		}
+		private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+		{
+			if (comboBox1.SelectedItem == "Software departement")
+			{
+				label4.Text = "Software Departement";
+				label17.Text = "قسم البرمجيات";
+				label15.Text = "ا.م.د.سرى زكي جاسم";
+				label16.Text = "رئيس قسم البرمجيات";
+			} 
+			if (comboBox1.SelectedItem == "Cybersecuirty deparetement")
+			{
+				label4.Text = "Cybersecurity Departement";
+				label17.Text = "قسم الامن السيبراني";
+				label15.Text = "أ.م.د.احمد خلفه عبيد";
+				label16.Text = "رئيس قسم الامن السيبراني";
+			}
+			if (comboBox1.SelectedItem == "networks departement")
+			{
+				label4.Text = "Networks Departememnt";
+				label17.Text = "قسم الشبكات";
+				label15.Text = "أ.م.د.الحارث عبد الكريم عبد الله";
+				label16.Text = "رئيس قسم الشبكات";
+			}
+		}
+	}
+}
